e0813e95f73a29a563073afb8eb229ff2d9121ef
Aancha14 <aancha.14.2025@bristol.ac.uk>
2026-07-21 15:37:03 +0100

    chnages in device

--- src/training/train.py
+++ src/training/train.py
@@ -28,8 +28,9 @@ def main():
     config = load_config(args.config)
 
     device = torch.device(
-        "mps" if torch.backends.mps.is_available()
-        else "cpu"
+    "cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
     )
 
     print(f"Using device: {device}")
